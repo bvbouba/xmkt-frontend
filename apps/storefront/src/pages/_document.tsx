5854c8683561bed3d801f066064e9d3f414502a3
Vaflaly Bakayoko <bvbouba@gmail.com>
2024-10-02 16:04:28 +0000

    added ico 1

--- apps/storefront/src/pages/_document.tsx
+++ apps/storefront/src/pages/_document.tsx
@@ -1,11 +1,13 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '/store'
+
   return (
     <Html lang="fr">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-
+        <link rel="icon" href={`${basePath}/ico.svg`} />
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
--- apps/webapp/src/pages/_document.tsx
+++ apps/webapp/src/pages/_document.tsx
@@ -1,9 +1,12 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '/marketing'
   return (
     <Html lang="fr">
-      <Head />
+      <Head>
+        <link rel="icon" href={`${basePath}/ico.svg`} />
+      </Head>
       <body>
         <Main />
         <NextScript />
