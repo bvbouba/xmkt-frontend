22ccaa9b084f9db43903d0daceea095e56eb2037
Vaflaly Bakayoko <bvbouba@gmail.com>
2024-10-03 02:09:57 +0000

    adding google verification

--- apps/storefront/src/pages/_document.tsx
+++ apps/storefront/src/pages/_document.tsx
@@ -1,10 +1,13 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { useRouter } from "next/router";
 
 export default function Document() {
   const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '/store'
-
+  const { locale } = useRouter();
+  const isFrench = locale === 'fr';
+  
   return (
-    <Html lang="fr">
+    <Html lang={locale}>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="icon" href={`${basePath}/ico.svg`} />
@@ -17,6 +20,22 @@ export default function Document() {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         />
+         {/* Conditional Meta Tags */}
+         {isFrench ? (
+          <>
+            <meta name="description" content="Jeu de simulation pour l'enseignement supérieur" />
+            <title>Jeux de simulation pour l'enseignement</title>
+            <meta property="og:title" content="Jeux de simulation engageants pour l'apprentissage dans l'enseignement supérieur" />
+            <meta property="og:description" content="Permettez à vos étudiants de pratiquer et d'apprendre grâce à des jeux de simulation interactifs conçus pour l'enseignement supérieur. Améliorez l'engagement et la compréhension avec nos solutions innovantes." />
+          </>
+        ) : (
+          <>
+            <meta name="description" content="Simulation Game for Higher Education" />
+            <title>Engaging Simulation Games for Learning in Education</title>
+            <meta property="og:title" content="Engaging Simulation Games for Learning in Higher Education" />
+            <meta property="og:description" content="Empower your students to practice and learn through interactive simulation games designed for higher education. Enhance engagement and understanding with our innovative solutions." />
+          </>
+        )}
       </Head>
       <body>
         <Main />
--- apps/webapp/src/pages/_document.tsx
+++ apps/webapp/src/pages/_document.tsx
@@ -1,11 +1,40 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { useRouter } from "next/router";
 
 export default function Document() {
   const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '/marketing'
+  const { locale } = useRouter();
+  const isFrench = locale === 'fr';
+
   return (
     <Html lang="fr">
       <Head>
         <link rel="icon" href={`${basePath}/ico.svg`} />
+        {isFrench ? (
+          <>
+
+            <meta name="description" content="Logiciel de simulation marketing conçu pour permettre aux étudiants et professionnels de pratiquer la prise de décision et de tester des théories dans un environnement compétitif." />
+            <title>Jeu de Simulation Marketing</title>
+
+            <meta property="og:title" content="Jeu de Simulation Marketing" />
+            <meta property="og:description" content="Participez à des simulations marketing réalistes pour aider les étudiants et professionnels à améliorer leurs compétences et à prendre des décisions dans un marché dynamique." />
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content="https://simuprof.ddns.com/marketing/" />
+
+          
+          </>
+        ) : (
+          <>
+            <meta name="description" content="Marketing simulation software designed for students and professionals to practice decision-making and test theories in a competitive environment." />
+            <title>Marketing Simulation Game</title>
+
+            <meta property="og:title" content="Marketing Simulation Game" />
+            <meta property="og:description" content="Engage in realistic marketing simulations that help students and professionals hone their skills and improve decision-making in a dynamic marketplace." />
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content="https://simuprof.ddns.com/marketing/" />
+          
+          </>
+        )}
       </Head>
       <body>
         <Main />
