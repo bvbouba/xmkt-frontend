bf265cdace310b2b13d9172bf29d92ee95fe09b7
Vaflaly Bakayoko <bvbouba@gmail.com>
2024-12-31 03:37:09 +0000

    update api-uri 1

--- packages/myconstants/src/index.ts
+++ packages/myconstants/src/index.ts
@@ -1,2 +1,2 @@
-export const API_URI = "https://ec2-13-60-11-115.eu-north-1.compute.amazonaws.com"
+export const API_URI = "https://ec2-13-60-11-115.eu-north-1.compute.amazonaws.com/"
 export const DEFAULT_LOCALE="fr"
